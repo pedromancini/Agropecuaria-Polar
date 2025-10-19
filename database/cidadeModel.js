9910f8cfc60a39a069a4fa8772cd2d57471bd075
mvster1 <thinklbs@protonmail.com>
2025-10-19 00:52:45 -0300

    fodase

--- database/cidadeModel.js
+++ database/cidadeModel.js
@@ -1,10 +1,24 @@
 const connection = require("./connection")
 const Sequelize = require("sequelize")
+const estadoModel = require("./estadoModel")
 
 cidadesModel = connection.define("CIDADE", {
-    idEnde
-
+    Nome: {
+        type: Sequelize.STRING,
+        allowNull: false, 
+    },
+    IdEstado: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: estadoModel,
+            key: "id"
+        }
+    }
 })
+
+// Associação 1->1 - Estado
+cidadesModel.belongsTo()
 
 cidadesModel.sync({force:false})
 
---
+++ database/estadoModel.js
@@ -0,0 +1,2 @@
+const connection = require("./connection")
+const Sequelize = require("sequelize")
